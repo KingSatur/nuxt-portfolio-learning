9225360cac0c5b9561278ed1442bf51c0a0221c7
Juan Campillo <codwithjuanda@gmail.com>
2024-03-27 12:47:20 -0500

    feat: creating pages and layout

---
+++ pages/blog/[blogId].vue
@@ -0,0 +1,8 @@
+<template>
+  <div>Blog page for id = {{ route.params.blogId }}</div>
+</template>
+
+<script setup>
+const route = useRoute();
+const { blogId } = route.params;
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Hello from page</div>
+</template>
